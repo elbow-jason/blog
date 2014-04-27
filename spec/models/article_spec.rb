da59525b8a0edb221b3dc6c062abcd581b34207a
Jason Goldberger <jlgoldb2@asu.edu>
2014-04-27 13:16:27 -0700

    for rick's curiosity

---
+++ spec/models/article_spec.rb
@@ -0,0 +1,25 @@
+require_relative '../spec_helper'
+
+
+describe Article do
+  let(:article){Article.new(title: 'myTest', 
+                            text: 'this is a text test')}
+
+  it 'works' do
+    expect(true).to be_truthy
+  end
+
+  it 'has a title longer than 5 and less than 24 chars' do 
+    expect(article.title).to eq('myTest')
+  end
+
+
+  it 'has a text' do
+    expect(article.text).to eq('this is a text test')
+  end
+
+  it 'checks strong params' do
+    article.permit?('fake_controller', 'index').to be_true
+  end
+
+end
